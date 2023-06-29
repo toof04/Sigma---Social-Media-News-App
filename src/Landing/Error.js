4525f5de32a9e2ebb15faf36b17ce741296723d2
Kartik Singh <kartiksingh389@gmail.com>
2023-06-29 18:18:26 +0530

    news api working - desc left

--- src/Landing/Error.js
+++ src/Landing/Error.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const Error = () => {
+import React, { useEffect, useState } from 'react';
+
+const NewsComponent = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=feef00c59ada4fbd9546a7c97ad0fe81'
+        );
+        const data = await response.json();
+        setArticles(data.articles.slice(0, 10)); // Get the first 10 articles
+      } catch (error) {
+        console.error('Error fetching news:', error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
   return (
     <div>
-      <h1>Error Page</h1>
-      <Link to='/' className='btn'>
-        Back Home
-      </Link>
+      <h1>Top 10 News Articles</h1>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <h2>{article.title}</h2>
+          <img src={article.urlToImage} alt="Article" />
+          <p>{article.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Error;
+export default NewsComponent;
--- src/Landing/Home.js
+++ src/Landing/Home.js
@@ -1,10 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import './Home.scss';
-import Post from './post.js';
+import './Section.scss';
+
 
 const Home = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
 
+  const sampleComments = [
+    {
+      username: 'Justin',
+      comment: 'This image is amazing!',
+    },
+    {
+      username: 'Justina',
+      comment: 'I love the colors in this picture.',
+    },
+    {
+      username: 'Justian',
+      comment: 'It makes me feel nostalgic.',
+    },
+  ];
+
+    const [showDescription, setShowDescription] = useState(false);
+
+  const handleButtonHover = () => {
+    setShowDescription(true);
+  };
+
+  const handleButtonLeave = () => {
+    setShowDescription(false);
+  };
+  const [scrollPosition, setScrollPosition] = useState(0);
+    const [articles, setArticles] = useState([]);
   const handleScroll = () => {
     const root = document.getElementById('root');
     const newScrollPosition = root.scrollTop;
@@ -15,9 +41,24 @@ const Home = () => {
   useEffect(() => {
     document.getElementById('root').addEventListener('scroll', handleScroll);
 
-    return () => {
-      document.getElementById('root').removeEventListener('scroll', handleScroll);
-    };
+    
+    
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=feef00c59ada4fbd9546a7c97ad0fe81'
+          );
+          const data = await response.json();
+          setArticles(data.articles.slice(0, 10)); // Get the first 10 articles
+        } catch (error) {
+          console.error('Error fetching news:', error);
+        }
+      };
+      
+      fetchNews();
+      return () => {
+        document.getElementById('root').removeEventListener('scroll', handleScroll);
+      };
   }, []);
 
   return (
@@ -29,9 +70,45 @@ const Home = () => {
        
         </center>
         <section className='feed'>
-          <div className='post'><Post /></div>
-          <div className='post'><Post /></div>
-          <div className='post'><Post /></div>
+          {articles.map((article, index) => (
+        <div key={index}>
+            <div className='post'>
+            <section className="section">
+ 
+                <div className="image-container a">
+                  <img src={article.urlToImage} alt="Article" className="image"/>
+
+                  <div className="emoji-options">
+                    <span className="emoji love">❤️</span>
+                    <span className="emoji sad">😢</span>
+                    <span className="emoji angry">😡</span>
+                  </div>
+                </div>
+                <div className="text-container b">
+                  <p className="image-text">
+                    {article.title}
+              </p>
+                        {showDescription && (
+          <div className="image-description">{article.description}</div>
+        )}
+             
+                </div>
+  <button className="comment-button c"      onMouseEnter={handleButtonHover}
+        onMouseLeave={handleButtonLeave}>Comment</button>
+      <div className="comments-container d">
+        {sampleComments.map((comment, index) => (
+          <p key={index} className="comment">
+            <span className="username">{comment.username}: </span>
+            {comment.comment}
+          </p>
+        ))}
+      </div>
+          </section>
+          </div>
+        </div>
+      ))}
+        
+
         </section>
       </section>
     </div>
