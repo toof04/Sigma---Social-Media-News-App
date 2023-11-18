2b13784f7b2dab16193610d90a7f302a0552e4c7
Kartik Singh <kartiksingh389@gmail.com>
2023-11-18 22:52:56 +0530

    Revert "dsa"
    This reverts commit 53f51a8411cfc32a137f249a0d6a14c2f183664b.

--- src/Landing/Navbar.js
+++ src/Landing/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
-// import logo from '../images/logo.svg'; 
+import logo from '../images/logo.svg'; 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import emailjs from '@emailjs/browser';
